ba7d03da4ecc52dd133a45a716f025baa837f711
anurag-verma-india <104461155+anurag-verma-india@users.noreply.github.com>
2025-08-27 01:37:39 +0530

    fix: qr path

--- qr/main.py
+++ qr/main.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
     # )
     create_coordinated_qr_image(
         my_link,
-        savetolocation=f"/storage/shared/01/qr-{current_time}.png",
+        savetolocation=f"/data/data/com.termux/files/home/storage/shared/01/qr-{current_time}.png",
         coordinates=False,
         # coordinates=True
     )
